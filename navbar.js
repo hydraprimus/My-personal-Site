7536cfd003464e7aced19346b2eecdce958f9cf3
Han-Yang <hyzhang121@gmail.com>
2026-07-23 15:06:38 -0700

    created html for and linked hobbies page to the home page

---
+++ navbar.js
@@ -0,0 +1,7 @@
+const hobbiesButton = document.querySelector('#hobbies')
+const interestsButton = document.querySelector('#interests')
+const contactButton = document.querySelector('#contact')
+
+hobbiesButton.addEventListener('click', () => {
+    window.location.href = 'hobbies.html'
+})
