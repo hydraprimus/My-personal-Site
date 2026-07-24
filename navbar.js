2efa07bfbee1a843f362226d9fbb60f0d108a601
Han-Yang <hyzhang121@gmail.com>
2026-07-23 17:33:05 -0700

    made all the navbar links clickable and styled hobbies page

--- navbar.js
+++ navbar.js
@@ -1,7 +1,20 @@
 const hobbiesButton = document.querySelector('#hobbies')
 const interestsButton = document.querySelector('#interests')
 const contactButton = document.querySelector('#contact')
+const homeButton = document.querySelector('#home')
 
 hobbiesButton.addEventListener('click', () => {
     window.location.href = 'hobbies.html'
+})
+
+homeButton.addEventListener('click', () => {
+    window.location.href = 'index.html'
+})
+
+interestsButton.addEventListener('click', () => {
+    window.location.href = 'interests.html'
+})
+
+contactButton.addEventListener('click', () => {
+    window.location.href = 'contact.html'
 })
